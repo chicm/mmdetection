28fed063e975b0bfd34720ff3c43d69c87fdf26b
chicm <idv2005@gmail.com>
2019-07-18 18:40:32 +0000

    balanced dataset

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -8,9 +8,10 @@ from .utils import to_tensor, random_scale, show_ann, get_dataset
 from .concat_dataset import ConcatDataset
 from .repeat_dataset import RepeatDataset
 from .extra_aug import ExtraAugmentation
+from .balanced_custom import BalancedCustomDataset
 
 __all__ = [
-    'CustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset', 'GroupSampler',
+    'CustomDataset', 'BalancedCustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset', 'GroupSampler',
     'DistributedGroupSampler', 'build_dataloader', 'to_tensor', 'random_scale',
     'show_ann', 'get_dataset', 'ConcatDataset', 'RepeatDataset',
     'ExtraAugmentation', 'WIDERFaceDataset'
