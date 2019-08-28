74ac7c5ab5a9fc1f770a5be99a6e80a3cb727f4a
chicm <idv2005@gmail.com>
2019-08-28 17:24:14 +0000

    seg

--- mmdet/datasets/__init__.py
+++ mmdet/datasets/__init__.py
@@ -12,11 +12,12 @@ from .balanced_custom import BalancedCustomDataset
 from .rel_custom import RelationCustomDataset
 from .rel_is_42 import RelationIs42CustomDataset
 from .rel_is_42_top import RelationIs42TopCustomDataset
+from .seg_custom import SegCustomDataset
 
 
 __all__ = [
     'CustomDataset', 'BalancedCustomDataset', 'RelationIs42CustomDataset', 'RelationCustomDataset', 'XMLDataset', 'CocoDataset', 'VOCDataset', 'GroupSampler',
     'DistributedGroupSampler', 'build_dataloader', 'to_tensor', 'random_scale',
     'show_ann', 'get_dataset', 'ConcatDataset', 'RepeatDataset',
-    'ExtraAugmentation', 'WIDERFaceDataset', 'RelationIs42TopCustomDataset'
+    'ExtraAugmentation', 'WIDERFaceDataset', 'RelationIs42TopCustomDataset', 'SegCustomDataset'
 ]
